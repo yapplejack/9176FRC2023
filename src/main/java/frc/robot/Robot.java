e726f456ab4246e5eece1694793acba07ee24387
yapplejack <jacklf@umich.edu>
2023-03-14 14:43:32 -0400

    inital commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,223 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
+import com.revrobotics.CANSparkMax.IdleMode;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.Constants.OIConstants;
+
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private Command m_autonomousCommand;
+
+  private RobotContainer m_robotContainer;
+
+  CANSparkMax arm = new CANSparkMax(9, MotorType.kBrushless);
+  CANSparkMax intake = new CANSparkMax(10, MotorType.kBrushless);
+
+
+  static final int ARM_CURRENT_LIMIT_A = 20;
+
+  /**
+   * Percent output to run the arm up/down at
+   */
+  static final double ARM_OUTPUT_POWER = 0.55;
+
+  /**
+   * How many amps the intake can use while picking up
+   */
+  static final int INTAKE_CURRENT_LIMIT_A = 25;
+
+  /**
+   * How many amps the intake can use while holding
+   */
+  static final int INTAKE_HOLD_CURRENT_LIMIT_A = 5;
+
+  /**
+   * Percent output for intaking
+   */
+  static final double INTAKE_OUTPUT_POWER = 1.0;
+
+  /**
+   * Percent output for holding
+   */
+  static final double INTAKE_HOLD_POWER = 0.07;
+
+  /**
+   * Time to extend or retract arm in auto
+   */
+  static final double ARM_EXTEND_TIME_S = 2.0;
+
+  /**
+   * Time to throw game piece in auto
+   */
+  static final double AUTO_THROW_TIME_S = 0.375;
+
+  /**
+   * Time to drive back in auto
+   */
+  static final double AUTO_DRIVE_TIME = 6.0;
+
+  /**
+   * Speed to drive backwards in auto
+   */
+  static final double AUTO_DRIVE_SPEED = -0.25;
+
+  XboxController m_manipController = new XboxController(OIConstants.kManipControllerPort);
+
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
+    arm.setInverted(true);
+    arm.setIdleMode(IdleMode.kBrake);
+    arm.setSmartCurrentLimit(ARM_CURRENT_LIMIT_A);
+    intake.setInverted(false);
+    intake.setIdleMode(IdleMode.kBrake);
+
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    /*
+     * String autoSelected = SmartDashboard.getString("Auto Selector",
+     * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
+     * = new MyAutoCommand(); break; case "Default Auto": default:
+     * autonomousCommand = new ExampleCommand(); break; }
+     */
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+  public void setArmMotor(double percent) {
+    arm.set(percent);
+  }
+
+  public void setIntakeMotor(double percent, int amps) {
+    intake.set(percent);
+    intake.setSmartCurrentLimit(amps);
+  }
+
+
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+    lastGamePiece = NOTHING;
+  }
+  static final int CONE = 1;
+  static final int CUBE = 2;
+  static final int NOTHING = 3;
+  int lastGamePiece;
+
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+    double armPower;
+    if (m_manipController.getRawButton(1)) {
+      // lower the arm
+      armPower = -ARM_OUTPUT_POWER;
+    } else if (m_manipController.getRawButton(4)) {
+      // raise the arm
+      armPower = ARM_OUTPUT_POWER;
+    } else {
+      // do nothing and let it sit where it is
+      armPower = 0.0;
+    }
+    setArmMotor(armPower);
+  
+    double intakePower;
+    int intakeAmps;
+    if (m_manipController.getRawButton(5)) {
+      // cube in or cone out
+      intakePower = INTAKE_OUTPUT_POWER;
+      intakeAmps = INTAKE_CURRENT_LIMIT_A;
+      lastGamePiece = CUBE;
+    } else if (m_manipController.getRawButton(6)) {
+      // cone in or cube out
+      intakePower = -INTAKE_OUTPUT_POWER;
+      intakeAmps = INTAKE_CURRENT_LIMIT_A;
+      lastGamePiece = CONE;
+    } else if (lastGamePiece == CUBE) {
+      intakePower = INTAKE_HOLD_POWER;
+      intakeAmps = INTAKE_HOLD_CURRENT_LIMIT_A;
+    } else if (lastGamePiece == CONE) {
+      intakePower = -INTAKE_HOLD_POWER;
+      intakeAmps = INTAKE_HOLD_CURRENT_LIMIT_A;
+    } else {
+      intakePower = 0.0;
+      intakeAmps = 0;
+    }
+    setIntakeMotor(intakePower, intakeAmps);
+
+  }
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+}
